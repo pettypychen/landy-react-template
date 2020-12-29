cb59b53006a601dfaf0547c712c4aac1f7b9d974
Petty PY Chen <39106042+pettypychen@users.noreply.github.com>
2020-12-29 21:13:51 +0800

    Relative path for homepage

--- my-app/src/common/Container/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import * as S from "./styles";
-
-const Container = (props) => (
-  <S.Container {...props}>{props.children}</S.Container>
-);
-
-export default Container;
